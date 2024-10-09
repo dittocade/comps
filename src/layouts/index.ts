6a508f75830f484fbb0d0c41a75829a188865032
bricked <git@bricked.dev>
2024-10-09 21:00:38 +0200

    feat(navlink): add navlink component

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header.astro";
+export { default as NavLink } from "./NavLink.astro";
 export { default as Footer } from "./Footer.astro";
 export { default as Disclaimer } from "./Disclaimer.astro";
