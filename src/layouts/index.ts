0599474a84729dcc48362fd904ca56684d596a88
bricked <git@bricked.dev>
2024-10-10 12:38:09 +0200

    feat(heading): add heading element

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,4 +1,5 @@
 export { default as Header } from "./Header.astro";
 export { default as NavLink } from "./NavLink.astro";
+export { default as Heading } from "./Heading.astro";
 export { default as Footer } from "./Footer.astro";
 export { default as Disclaimer } from "./Disclaimer.astro";
